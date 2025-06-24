fc9b6cf1dc01cb2f463b2de3349051215204389d
Muhammad Arif Wicaksana <muarwi@gmail.com>
2025-06-24 12:50:38 +0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,10 +13,10 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "wicaksana_Id"
 
     workspaces {
-      name = "sentinel-example"
+      name = "learn-terraform-sentinel"
     }
   }
 
